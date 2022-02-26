fdec9512dc965d9ad1d2239f562ee1cc1fd6af4e
adrian.soomro <adrian.soomro@bjss.com>
2022-02-26 10:37:10 +0000

    format config file

--- buildandreleasetask/jest.config.js
+++ buildandreleasetask/jest.config.js
@@ -1,25 +1,25 @@
 module.exports = {
-  moduleFileExtensions: ["ts", "tsx", "js"],
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    '^.+\\.ts$': 'ts-jest'
   },
-  testMatch: ["**/*.(test|spec).(ts|tsx)"],
+  testMatch: ['**/*.(test).(ts|tsx)'],
   globals: {
-    "ts-jest": {
+    'ts-jest': {
       babelConfig: true,
-      tsconfig: "tsconfig.json",
-      compiler: "ttypescript",
-    },
+      tsconfig: 'tsconfig.json',
+      compiler: 'ttypescript'
+    }
   },
-  coveragePathIgnorePatterns: ["/node_modules/"],
-  coverageReporters: ["json", "text", "text-summary", "cobertura", "lcov"],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageReporters: ['json', 'text', 'text-summary', 'cobertura', 'lcov'],
   reporters: [
     // This enables console output for local development and build log output in Pipelines.
-    "default",
+    'default'
   ],
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__mocks__/mocks.js",
-    "\\.(css|less)$": "<rootDir>/__mocks__/mocks.js",
-  },
-};
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/mocks.js',
+    '\\.(css|less)$': '<rootDir>/__mocks__/mocks.js'
+  }
+}
