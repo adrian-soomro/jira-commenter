59962df091d2ccbe823e81a8e7c8383b653c6b0f
Adrian Soomro <37105695+adrian-soomro@users.noreply.github.com>
2022-02-26 10:54:28 +0000

    Feat/improve testing (#4)
    * format config file
    
    * remove component like test
    
    * improve test output format
    
    * fix sloppy copy and paste job
    
    Co-authored-by: adrian.soomro <adrian.soomro@bjss.com>

--- buildandreleasetask/test/mockRunner.ts
+++
@@ -1,6 +0,0 @@
-import * as fs from 'fs'
-import { setupTaskMockRunner } from './testUtils'
-
-const inputParams = JSON.parse(fs.readFileSync('../tmp/testData.json', 'utf8'))
-
-setupTaskMockRunner(...inputParams)
